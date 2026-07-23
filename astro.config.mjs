9855c8f90d575fbe0538a0999d33653e03bfe28c
Deanna Thompson <59544150+itsdeannat@users.noreply.github.com>
2026-07-23 12:07:08 -0400

    chore: update dependency files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
@@ -6,19 +5,37 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Fstop API Docs',
+			social: [
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/itsdeannat/fstop-api-docs.git'},
+			],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Start here',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'About the API', slug: 'guides/about-the-api'},
+						{ label: 'Getting started', slug: 'guides/getting-started' }
 					],
 				},
 				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
+					label: 'Feature guides',
+					items: [
+						{ label: 'Authentication', slug: 'guides/authentication' },
+				// 		{ label: 'Get all clients', slug: 'guides/get-all-clients' },
+				// 		{ label: 'Get a client', slug: 'guides/get-client' },
+				// 		{ label: 'Create a client', slug: 'guides/create-client' },
+				// 		{ label: 'Get all projects', slug: 'guides/get-all-projects' },
+				// 		{ label: 'Get a project by client', slug: 'guides/get-project-by-client' },
+				// 		{ label: 'Create a project', slug: 'guides/create-project' },
+				// 		{ label: 'Get all bookings', slug: 'guides/get-all-bookings' },
+				// 		{ label: 'Get a booking by project', slug: 'guides/get-booking-by-project' },
+				// 		{ label: 'Create a booking', slug: 'guides/create-booking' }
+					]
+				},
+				{
+					label: 'API reference',
+					items: [{ autogenerate: { "directory": "reference" } }]
 				},
 			],
 		}),
