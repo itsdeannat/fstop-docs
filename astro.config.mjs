b182c2fc9b21927fdee0dc74b7f2e4d12342999b
Deanna Thompson <59544150+itsdeannat@users.noreply.github.com>
2026-07-25 20:40:49 -0400

    docs: add feature guides

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,15 +22,15 @@ export default defineConfig({
 					label: 'Feature guides',
 					items: [
 						{ label: 'Authentication', slug: 'guides/authentication' },
-				// 		{ label: 'Get all clients', slug: 'guides/get-all-clients' },
-				// 		{ label: 'Get a client', slug: 'guides/get-client' },
-				// 		{ label: 'Create a client', slug: 'guides/create-client' },
-				// 		{ label: 'Get all projects', slug: 'guides/get-all-projects' },
-				// 		{ label: 'Get a project by client', slug: 'guides/get-project-by-client' },
-				// 		{ label: 'Create a project', slug: 'guides/create-project' },
-				// 		{ label: 'Get all bookings', slug: 'guides/get-all-bookings' },
-				// 		{ label: 'Get a booking by project', slug: 'guides/get-booking-by-project' },
-				// 		{ label: 'Create a booking', slug: 'guides/create-booking' }
+						{ label: 'Get all clients', slug: 'guides/get-all-clients' },
+						{ label: 'Get a client by ID', slug: 'guides/get-a-client-by-id' },
+						{ label: 'Create a client', slug: 'guides/create-a-client' },
+						{ label: 'Get all projects', slug: 'guides/get-all-projects' },
+						{ label: 'Get a project by client', slug: 'guides/get-a-project-by-client' },
+						{ label: 'Create a project', slug: 'guides/create-a-project' },
+						{ label: 'Get all bookings', slug: 'guides/get-all-bookings' },
+						{ label: 'Get a booking by project', slug: 'guides/get-a-booking-by-project' },
+						{ label: 'Create a booking', slug: 'guides/create-a-booking' }
 					]
 				},
 				{
